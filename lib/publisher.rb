858611eb1230d1d31fafc32262e4279f68dfa608
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-02 10:30:13 -0500

    call in new deps

--- lib/publisher.rb
+++ lib/publisher.rb
@@ -1,11 +1,15 @@
 require "publisher/util"
 require "publisher/engine"
 require "publisher/publish_initializer"
-require "publisher/rabl_inspector"
-require "publisher/rabl_dsl"
 require "publisher/push_resource"
 require "publisher/push_to_parent"
-require "publisher/macros"
+require "publisher/macros" # TODO get rid of
+require "publisher/rabl_engine_extension"
+
+require "publisher/dataview_tree/dataview_node.rb"
+require "publisher/dataview_tree/dataview_root_node.rb"
+require "publisher/dataview_tree/rabl_dsl.rb"
+require "publisher/dataview_tree/rabl_inspector"
 
 module Publisher
 

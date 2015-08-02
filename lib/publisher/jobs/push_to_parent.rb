a5dc0ec4a3f2798e678b24992c2d6a0bb5647653
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-02 14:44:25 -0500

    redo job with dataviews

--- lib/publisher/jobs/push_to_parent.rb
+++ lib/publisher/jobs/push_to_parent.rb
@@ -4,25 +4,14 @@ module Publisher
 
     def self.perform(resource_id, resource_class, requested_at)
       resource = resource_class.constantize.find(resource_id)
-      call_chains(resource.class).each do |chain|
-        obj = nil
-        obj = resource if chain.length > 0
-        chain.each do |method|
-          obj = obj.send(method)
-        end
-        Resque.enqueue(Publisher::PushResource, obj.id, obj.class.to_s, requested_at) if obj
+      dataviews = Publisher.child_publish[resource_class]
+      dataviews.each do |dataview|
+        root_resource = dataview.find_parent(resource)
+        raise RuntimeException unless root_resource
+        root_resource_id = root_resource.id
+        root_class = dataview.root_class
+        Resque.enqueue(Publisher::PushResource, root_resource_id, root_class, requested_at) if root_resource_id # TODO: consider exception here
       end
-
     end
-
-    # looks for call chain of class and all relevant super classes
-    def self.call_chains(klass)
-      super_klasses(klass).each do |klass|
-        chains = Publisher.child_lookup_hash[klass.to_s]
-        return chains if chains
-      end
-      []
-    end
-
   end
 end

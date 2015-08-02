19b4da5db7f9a3aa734e766fbd293e1febb0d549
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-02 10:32:07 -0500

    redo initializer with new rabl analysis classes

--- lib/publisher/publish_initializer.rb
+++ lib/publisher/publish_initializer.rb
@@ -1,65 +1,47 @@
 module Publisher
   class PublishInitializer
-
     class << self
 
       def glob_files
         Dir.glob('app/publish/*.rabl').map {|f| f[/app\/publish\/(.*)\.rabl/,1] }
       end
 
-      def get_lookup_chain
-        root_publish_hash = {}
-        glob_files.each do |file_name|
-          klass = RablDsl.get_klass(file_name)
-          root_publish_hash[klass] = RablInspector.process_template(file_name)
+      def root_publish
+        @root_publish ||= begin
+          root_dataviews_hash = {}
+          root_dataviews.each do |root_view|
+            root_dataviews_hash[root_view.klass] ||= []
+            root_dataviews_hash[root_view.klass] << root_view
+          end
+          root_dataviews_hash
         end
-        convert_to_call_chain(root_publish_hash)
       end
 
-      def convert_to_call_chain(root_hash)
-        call_chain_hash = {}
-        root_hash.each do |root_obj,v|
-          v.each do |level, descs|
-            descs.each do |desc|
-              call_array = []
-              get_child_chain(desc, call_array)
-              merge_chain = unfold_chain(call_array, root_obj)
-              call_chain_hash.merge!(merge_chain)
-            end
+      def child_publish
+        @child_publish ||= begin
+          child_dataviews_hash = {}
+          root_dataviews.each do |root_view|
+            root_view.children.each { |child_view| mark_child(child_view, child_dataviews_hash) }
+          end
+          child_dataviews_hash
+        end
+      end
+
+      def root_dataviews
+        @root_dataviews ||= begin
+          glob_files.map do |file_name|
+            RablInspector.process_template(file_name)
           end
         end
-        flatten_call_chain(call_chain_hash)
       end
+      private :root_dataviews
 
-      def flatten_call_chain(call_chain)
-        flat_chain = {}
-        call_chain.each do |mapping_hash, method_array|
-          flat_chain[mapping_hash.keys.first] ||= []
-          flat_chain[mapping_hash.keys.first] << method_array
-        end
-        flat_chain
+      def mark_child(child_view, hash)
+        hash[child_view.klass] ||= []
+        hash[child_view.klass] << child_view
+        child_view.children.each { |cdv| mark_child(cdv, hash) }
       end
-
-      def get_child_chain(obj_hash, call_array)
-        call_array << { obj_hash[:reverse_method] => obj_hash[:target_klass] }
-        if(obj_hash[:parent])
-          get_child_chain(obj_hash[:parent], call_array)
-        end
-      end
-
-      def unfold_chain(call_array, root_obj)
-        merge_hash = {}
-        call_array.each do |method_obj|
-          method = method_obj.keys.first
-          publish_obj = method_obj.values.first
-          # circular references not allowed in rabl
-          merge_hash.each do |obj_root_hash, method_array|
-            method_array << method
-          end
-          merge_hash[{ publish_obj.to_s => root_obj.to_s }] = [method]
-        end
-        merge_hash
-      end
+      private :mark_child
 
     end
   end

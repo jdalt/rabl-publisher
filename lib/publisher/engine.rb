04028fd5f99009f00d95e79a3a78efccfa4f5b0c
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-02 14:10:44 -0500

    use callback binder

--- lib/publisher/engine.rb
+++ lib/publisher/engine.rb
@@ -4,14 +4,10 @@ module Publisher
   class Engine < ::Rails::Engine
     isolate_namespace Publisher
 
-    # defaults; may want to provide mechanism to override via yaml in host app
-    config.before_initialize do |app|
-      Publisher.root_objects = PublishInitializer.glob_files.map(&:camelize)
-    end
-
     config.after_initialize do |app|
-      Publisher.child_publish = PublishInitializer.child_publish
       Publisher.root_publish = PublishInitializer.root_publish
+      Publisher.child_publish = PublishInitializer.child_publish
+      Publisher::Callbacks.bind(Publisher.root_publish, Publisher.child_publish)
     end
 
   end

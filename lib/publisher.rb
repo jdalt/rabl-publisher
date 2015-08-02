475762c45187f5359ed77f21cf9c11f22037db0a
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-02 12:40:44 -0500

    move jobs to subdir

--- lib/publisher.rb
+++ lib/publisher.rb
@@ -1,10 +1,11 @@
 require "publisher/util"
 require "publisher/engine"
 require "publisher/publish_initializer"
-require "publisher/push_resource"
-require "publisher/push_to_parent"
 require "publisher/macros" # TODO get rid of
 require "publisher/rabl_engine_extension"
+
+require "publisher/jobs/push_resource"
+require "publisher/jobs/push_to_parent"
 
 require "publisher/dataview_tree/dataview_node.rb"
 require "publisher/dataview_tree/dataview_root_node.rb"
---
+++ lib/publisher/jobs/push_resource.rb
@@ -0,0 +1,29 @@
+module Publisher
+  class PushResource
+    extend Publisher::Util
+    @queue = :high
+
+    def self.perform(resource_id, resource_class, requested_at)
+      # CONSIDER: we can check requested_at XOR use sliding window or other job
+      # strategy to prevent multiple requests to publish the same job by
+      # multiple children.
+      resource = resource_class.constantize.find(resource_id)
+      dataviews = Publisher.root_publish[resource_class]
+      # CONSIDER: exception of log warning if no dataviews found for class?
+      # Lean exception.
+      dataviews.each do |dataview|
+        payload = Rabl.render(resource, dataview.source_template, :view_path => 'app/publish', :format => :json)
+        res = Typhoeus.post("localhost:3000/publish", body: payload, headers: {'Content-Type' => 'application/json'})
+        # use exponential stand off if failed?
+        puts res.request.response.body
+      end
+      nil
+    end
+
+    # TODO: handle super classes OR is binding to callbacks of base class
+    # sufficient?
+    def self.super_klasses_snake(klass)
+      super_klasses(klass).map { |klazz| klazz.to_s.underscore }
+    end
+  end
+end
